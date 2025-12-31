de61a17639b8d59b016e66e97447e8f94652b802
Gagandeep <bsgagandeep5@gmail.com>
2025-12-31 18:53:15 +0530

    Initial backend setup commit

---
+++ MobileCare/src/main/java/com/example/MobileCare/MobileCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.MobileCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileCareApplication.class, args);
+	}
+
+}
---
+++ MobileCare/src/test/java/com/example/MobileCare/MobileCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MobileCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
